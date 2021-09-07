3692e0ecbe20cf44af86f6920345092974aaac14
Seda Movsisyan <smovsisyan@zscaler.com>
2021-09-07 12:38:22 +0400

    Classwork for lesson 3

--- src/classwork/lesson3/Classwork3.java
+++ src/classwork/lesson3/Classwork3.java
@@ -5,25 +5,23 @@ import java.util.Scanner;
 public class Classwork3 {
 
     public static void main(String[] args) {
-//        Scanner inputScanner = new Scanner(System.in);
-//        System.out.println("Enter your number 1.");
-//        int a = inputScanner.nextInt();
-//        System.out.println("You entered: " + a);
-//        int b = inputScanner.nextInt();
-//        System.out.println("You entered: " + b);
-//        int c = inputScanner.nextInt();
-//        System.out.println("You entered: " + c);
-//        int sum = a + b + c;
-//        System.out.println("The sum is: " + (a + b + c));
-//        System.out.println("Enter your string value: ");
-//        String inputStr1 = inputScanner.next();
-//        boolean hasNextFirst = inputScanner.hasNext();
-//        System.out.println("Has next arg: " + hasNextFirst);
-//        boolean hasNextSecond = inputScanner.hasNext();
-//        System.out.println("Has next arg: " + hasNextSecond);
-//        System.out.println("You entered : " + inputStr1);
-        System.out.println(2);
-
+        Scanner inputScanner = new Scanner(System.in);
+        System.out.println("Enter your number 1.");
+        int a = inputScanner.nextInt();
+        System.out.println("You entered: " + a);
+        int b = inputScanner.nextInt();
+        System.out.println("You entered: " + b);
+        int c = inputScanner.nextInt();
+        System.out.println("You entered: " + c);
+        int sum = a + b + c;
+        System.out.println("The sum is: " + (a + b + c));
+        System.out.println("Enter your string value: ");
+        String inputStr1 = inputScanner.next();
+        boolean hasNextFirst = inputScanner.hasNext();
+        System.out.println("Has next arg: " + hasNextFirst);
+        boolean hasNextSecond = inputScanner.hasNext();
+        System.out.println("Has next arg: " + hasNextSecond);
+        System.out.println("You entered : " + inputStr1);
 
         if (args.length > 0) {
             System.out.print("First arg is: " + args[0]);
